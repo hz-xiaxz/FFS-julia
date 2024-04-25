d9f1fcf13a24e5219d9ddc462840039219219239
hz-xiaxz <1806656034@qq.com>
2024-04-25 23:03:38 +0800

    intial commit

--- src/FastFermionSampling.jl
+++ src/FastFermionSampling.jl
@@ -1,5 +1,18 @@
 module FastFermionSampling
 
 # Write your package code here.
+using Random
+using LinearAlgebra
+
+export FFS
+
+function FFS(u::Matrix{ComplexF64}, L::Int, N::Int)
+    # U: Matrix{ComplexF64}: the sampling ensemble
+    # L: Int: the number of energy states
+    # N: Int: the number of fermions
+    v = randperm(N)
+    U = u[:,v]
+    # For x1 case, P(x1;m) = |U_{x1, m1}|^2 
+end
 
 end
