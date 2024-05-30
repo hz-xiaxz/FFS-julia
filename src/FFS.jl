68c220c2e2e4938affcadd10206a33ee6b7419ac
hz-xiaxz <1806656034@qq.com>
2024-05-30 17:09:43 +0800

    improved

--- src/FFS.jl
+++ src/FFS.jl
@@ -1,7 +1,11 @@
+"""
+Employing Fast Fermion Sampling Algorithm to sample free Fermions
+
+FFS([rng=default_rng()], U::AbstractMatrix)
+
+U: the sampling ensemble, a matrix of size L x N, where L is the number of energy states and N is the number of Fermions 
+"""
 function FFS(r::AbstractRNG, u::AbstractMatrix)
-    """
-    U: Matrix{Float64}: the sampling ensemble
-    """
     L, N = size(u)
     v = randperm(r, N)
     U = u[:, v]
@@ -14,8 +18,8 @@ function FFS(r::AbstractRNG, u::AbstractMatrix)
     sampled[x_new] = true
     avail[x_new] = false
     n_vec = normalize([-U[x_new, 2] / U[x_new, 1], 1])
-    for i in 2:N-1
-        prob = abs2.(view(U, avail, 1:i) * n_vec)
+    @inbounds for i in 2:N-1
+        prob = abs2.((view(U, :, 1:i)*n_vec)[avail])
         x_new = sample(r, groud_set[avail], Weights(prob))
         sampled[x_new] = true
         avail[x_new] = false
@@ -25,7 +29,7 @@ function FFS(r::AbstractRNG, u::AbstractMatrix)
         B = -U[1:i, i+1]
         n_vec = normalize([U_x \ B; 1])
     end
-    prob = abs2.(view(U, avail, 1:N) * n_vec)
+    prob = abs2.((view(U, :, 1:N)*n_vec)[avail])
     x_new = sample(r, groud_set[avail], Weights(prob))
     sampled[x_new] = true
     avail[x_new] = false
