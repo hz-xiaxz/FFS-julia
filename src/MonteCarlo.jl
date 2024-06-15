5c2d5fb9a38627aeee3952f297a9ea782ad9b08b
hz-xiaxz <1806656034@qq.com>
2024-06-15 17:51:23 +0800

    Carlo successfully run without mpi (benchmark needed)

--- src/MonteCarlo.jl
+++ src/MonteCarlo.jl
@@ -18,7 +18,16 @@ Create a Monte Carlo object
 function MC(params::AbstractDict)
     nx = params[:nx]
     ny = params[:ny]
-    return MC{B}(BitVector(zeros(2 * nx * ny)))
+    if params[:B] == "Periodic"
+        B = Periodic()
+    elseif params[:B] == "Open"
+        B = Open()
+    else
+        throw(ArgumentError("Boundary condition not recognized"))
+    end
+    lat = LatticeRectangular(params[:nx], params[:ny], B)
+    model = AHmodel(lat, params[:t], params[:W], params[:U], params[:N_up], params[:N_down])
+    return MC{B}(model, BitVector(zeros(2 * nx * ny)))
 end
 
 
@@ -27,32 +36,32 @@ end
 ------------
 Initialize the Monte Carlo object
 `params`
-* nx : `Int` number of sites in x direction
-* ny : `Int` number of sites in y direction
-* `B`: `AbstractBoundary` boundary condition, `Periodic` or `Open`
-* t : `Float64` hopping parameter
-* W : `Float64` disorder strength
-* U : `Float64` on-site interaction strength
-* N_up : `Int` number of up spins
-* N_down : `Int` number of down spins
+* `nx` : `Int` number of sites in x direction
+* `ny` : `Int` number of sites in y direction
+* `B` : `AbstractBoundary` boundary condition, `Periodic` or `Open`
+* `t` : `Float64` hopping parameter
+* `W` : `Float64` disorder strength
+* `U` : `Float64` on-site interaction strength
+* `N_up` : `Int` number of up spins
+* `N_down` : `Int` number of down spins
 """
 function Carlo.init!(mc::MC{B}, ctx::MCContext, params::AbstractDict) where {B}
-    lat = LatticeRectangular(params[:nx], params[:ny], params[:B])
+    lat = LatticeRectangular(params[:nx], params[:ny], B)
     orb = AHmodel(lat, params[:t], params[:W], params[:U], params[:N_up], params[:N_down])
-    conf = hcat(FFS(ctx.rng, orb.U_up), FFS(ctx.rng, orb.U_down))
+    conf = vcat(FFS(ctx.rng, orb.U_up), FFS(ctx.rng, orb.U_down))
     mc.model = orb
     mc.conf = conf
 end
 
 function Carlo.sweep!(mc::MC{B}, ctx::MCContext) where {B}
-    mc.conf = hcat(FFS(ctx.rng, mc.model.U_up), FFS(ctx.rng, mc.model.U_down))
+    mc.conf = vcat(FFS(ctx.rng, mc.model.U_up), FFS(ctx.rng, mc.model.U_down))
     return nothing
 end
 
-function Carlo.measure!(mc::MC{B}, ctx::MCContext) where {B}    
+function Carlo.measure!(mc::MC{B}, ctx::MCContext) where {B}
     conf_up = FFS(ctx.rng, mc.model.U_up)
     conf_down = FFS(ctx.rng, mc.model.U_down)
-    g = mc.model.g
+    g = 1.0 # temporarily fixed
     Gutz = Gutzwiller(mc.model, conf_up, conf_down, g)
     measure!(ctx, :OL, Gutz.OL)
     return nothing
@@ -64,11 +73,11 @@ end
 
 
 function Carlo.write_checkpoint(mc::MC{B}, out::HDF5.Group) where {B}
-    out["conf"] = mc.conf
+    out["conf"] = Vector{Bool}(mc.conf)
     return nothing
 end
 
 function Carlo.read_checkpoint!(mc::MC{B}, in::HDF5.Group) where {B}
-    mc.conf = read(in, "conf")
+    mc.conf = BitVector(read(in, "conf"))
     return nothing
 end
