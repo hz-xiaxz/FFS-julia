68f6f4263d9d8122bbde53538920fb65c870ac9e
hz-xiaxz <1806656034@qq.com>
2024-06-25 13:42:07 +0800

    hack MC to avoid problem mentioned in #21

--- examples/naiveMC.jl
+++ examples/naiveMC.jl
@@ -4,13 +4,22 @@ using FastFermionSampling
 
 function run(MCsteps::Int)
     lat = LatticeRectangular(4, 4, Periodic())
-    orb = AHmodel(lat, 1.0, 1.0, 1.0, 8, 8)
-    sm = Vector{Float64}(undef, MCsteps)
+    orb = FastFermionSampling.fixedAHmodel(lat, 1.0, 1.0, 1.0, 8, 8)
+    sm = zeros(Float64, MCsteps)
     for i in 1:MCsteps
         conf_up = FFS(orb.U_up)
         conf_down = FFS(orb.U_down)
         gut = Gutzwiller(orb, conf_up, conf_down, 1.0)
-        sm[i] = gut.OL
+        # HACK!!!
+        if i > 1 
+            if abs(gut.OL) > abs(10 * mean(sm[1:i]))
+                sm[i] = mean(sm[1:i])
+            else
+                sm[i] = gut.OL
+            end
+        else
+            sm[i] = gut.OL
+        end
     end
     return mean(sm), (std(sm) / √(MCsteps - 1))
 end
