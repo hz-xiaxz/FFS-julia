8781a73942f3a6a2d730a0de635c08faab5a75bc
LeoXia <114814844+hz-xiaxz@users.noreply.github.com>
2024-07-18 17:19:57 +0800

    ED (#33)
    * add ED with onsite disorder

--- src/ED.jl
+++ src/ED.jl
@@ -2,12 +2,16 @@ using QuantumLattices
 using ExactDiagonalization
 using LinearAlgebra: eigen
 
-function doED(Lx::Int, Ly::Int, t::Float64, U::Float64)
+
+function doED(Lx::Int, Ly::Int, t::Float64, U::Float64, omega::Vector{Float64}, boundary::Char)
+    @assert length(omega) == Lx * Ly "onsite disorder should be the same as the number of sites, got $(length(omega)) and $(Lx * Ly)"
+    global omega_reshape = reshape(omega, Lx, Ly)
+
     # define the unitcell of the square lattice
     unitcell = Lattice([0.0, 0.0]; name=:Square, vectors=[[1.0, 0.0], [0.0, 1.0]])
 
     # define a finite 3×4 cluster of the square lattice with open boundary condition
-    boundary = ntuple(i -> 'P', 2)
+    boundary = ntuple(i -> boundary,2)
     lattice = Lattice(unitcell, (Lx, Ly), boundary)
 
     # define the Hilbert space (single-orbital spin-1/2 complex fermion)
@@ -18,11 +22,25 @@ function doED(Lx::Int, Ly::Int, t::Float64, U::Float64)
     quantumnumber = SpinfulParticle(length(lattice), 0)
 
     # define the terms, i.e. the nearest-neighbor hopping and the Hubbard interaction
-    t = Hopping(:t, -t, 1)
-    U = Hubbard(:U, U)
+    t_term = Hopping(:t, -t, 1)
+    U_term = Hubbard(:U, U)
+
+    #########################
+
+    function disorder(bond::Bond)
+        coordinate = bond.points[1].rcoordinate
+        onsite = omega_reshape[Int(coordinate[1])+1, Int(coordinate[2])+1]
+        return onsite
+    end
+
+    ω_up = Term{:Onsite}(:ωup, 1.0, 0, Coupling(Index(:, FID{:f}(:, 1 // 2, 2)), Index(:, FID{:f}(:, 1 // 2, 1))), true; amplitude=disorder, ismodulatable=true)
+
+    ω_down = Term{:Onsite}(:ωdown, 1.0, 0, Coupling(Index(:, FID{:f}(:, -1 // 2, 2)), Index(:, FID{:f}(:, -1 // 2, 1))), true; amplitude=disorder, ismodulatable=true)
+
+    ###########################
 
     # define the exact diagonalization algorithm for the Fermi Hubbard model
-    ed = ED(lattice, hilbert, (t, U), quantumnumber)
+    ed = ED(lattice, hilbert, (t_term, U_term, ω_up, ω_down), quantumnumber)
 
     # find the ground state and its energy
     eigensystem = eigen(ed; nev=1)
--- src/FastFermionSampling.jl
+++ src/FastFermionSampling.jl
@@ -17,5 +17,6 @@ include("Orbitals.jl")
 include("FFS.jl")
 include("Ansatz.jl")
 include("MonteCarlo.jl")
+include("ED.jl")
 
 end
