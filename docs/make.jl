0098b54511f8a2609482c14aec002019bb6091d9
hz-xiaxz <22307110069@m.fudan.edu.cn>
2024-07-25 12:57:48 +0800

    no format check

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,25 @@
 using FastFermionSampling
 using Documenter
 
-DocMeta.setdocmeta!(FastFermionSampling, :DocTestSetup, :(using FastFermionSampling); recursive=true)
+DocMeta.setdocmeta!(
+    FastFermionSampling, :DocTestSetup, :(using FastFermionSampling); recursive = true)
 
 makedocs(;
-    modules=[FastFermionSampling],
-    authors="XuanzheXia",
-    sitename="FFS-julia",
-    format=Documenter.HTML(;
-        canonical="https://hz-xiaxz.github.io/FFS-julia",
-        edit_link="main",
-        assets=String[],
+    modules = [FastFermionSampling],
+    authors = "XuanzheXia",
+    sitename = "FFS-julia",
+    format = Documenter.HTML(;
+        canonical = "https://hz-xiaxz.github.io/FFS-julia",
+        edit_link = "main",
+        assets = String[]
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = [
+        "Home" => "index.md"
+    ]
 )
 
 deploydocs(;
-    repo="github.com/hz-xiaxz/FFS-julia.git",
-    devbranch="main",
-    push_preview=true,
+    repo = "github.com/hz-xiaxz/FFS-julia.git",
+    devbranch = "main",
+    push_preview = true
 )
