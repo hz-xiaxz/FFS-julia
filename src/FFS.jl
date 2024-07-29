56d1747d238325ce6f7d276dddfd8a80bfebc434
hz-xiaxz <1806656034@qq.com>
2024-07-30 00:36:25 +0800

    save, change all bitstring to BitStr

--- src/FFS.jl
+++ src/FFS.jl
@@ -6,7 +6,6 @@ Employing Fast Fermion Sampling Algorithm to sample free Fermions
 `U` : the sampling ensemble, a matrix of size `L` x `N`, where `L` is the number of energy states and `N` is the number of Fermions 
 """
 function FFS(r::AbstractRNG, u::AbstractMatrix)
-    # TODO: change to BitStr 
     L, N = size(u)
     v = randperm(r, N)
     U = u[:, v]
@@ -38,3 +37,38 @@ function FFS(r::AbstractRNG, u::AbstractMatrix)
 end
 
 FFS(u::AbstractMatrix) = FFS(Random.default_rng(), u)
+
+
+function newFFS(r::AbstractRNG, u::AbstractMatrix)
+    L, N = size(u)
+    v = randperm(r, N)
+    U = u[:, v]
+    bstr_type = L <= 64 ? Int64 : BitBasis.LongLongUInt{L ÷ 64 + 1}
+    sampled = BitStr{L}(0)
+    avail = trues(L)
+    groud_set = collect(1:L)
+    # For x1 case, P(x1;m) = |U_{x1, m1}|^2 
+    p = abs2.(U[:, 1])
+    x_new = sample(r, 1:L, Weights(p))
+    sampled |= indicator(bstr_type, x_new)
+    avail[x_new] = false
+    n_vec = normalize([-U[x_new, 2] / U[x_new, 1], 1])
+    @inbounds for i in 2:(N - 1)
+        prob = abs2.((view(U, :, 1:i) * n_vec)[avail])
+        x_new = sample(r, groud_set[avail], Weights(prob))
+        sampled |= indicator(bstr_type, x_new)
+        avail[x_new] = false
+        # now compute next n_vec
+        # I suggest not using the gaussian elimination
+        sampled_bool = @. ~ avail
+        U_x = U[sampled_bool, 1:i]
+        B = -U[1:i, i + 1]
+        n_vec = normalize([U_x \ B; 1])
+    end
+    prob = abs2.((view(U, :, 1:N) * n_vec)[avail])
+    x_new = sample(r, groud_set[avail], Weights(prob))
+    sampled |= indicator(bstr_type, x_new)
+    return sampled
+end
+
+newFFS(u::AbstractMatrix) = newFFS(Random.default_rng(), u)
