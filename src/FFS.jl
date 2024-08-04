6c0568603fe6a8900b69ebc5fee29a910b09c1a6
hz-xiaxz <1806656034@qq.com>
2024-08-04 14:24:57 +0800

    save, negative optimization

--- src/FFS.jl
+++ src/FFS.jl
@@ -1,4 +1,10 @@
+using Random
+using LinearAlgebra
+using StatsBase
+using BitBasis
+
 """
+    FFS(U::AbstractMatrix)
     FFS([rng=default_rng()], U::AbstractMatrix)
     
 Employing Fast Fermion Sampling Algorithm to sample free Fermions
@@ -38,37 +44,53 @@ end
 
 FFS(u::AbstractMatrix) = FFS(Random.default_rng(), u)
 
+# add wrapper structs to define Bool-vector like to_index function for BitStr
+struct AvailConf{D, N, T <: Integer}
+    ref::Base.RefValue{DitStr{D, N, T}}
+end
+
+struct SampledConf{D, N, T <: Integer}
+    ref::Base.RefValue{DitStr{D, N, T}}
+end
+
+function Base.to_index(d::SampledConf{D, N, T}) where {D, N, T}
+    return Base.LogicalIndex(map(i -> readbit(d.ref[], i) == 1, 1:N))
+end
+
+
+function Base.to_index(d::AvailConf{D, N, T}) where {D, N, T}
+    return Base.LogicalIndex(map(i -> readbit(d.ref[], i) == 0, 1:N))
+end
+
 
 function newFFS(r::AbstractRNG, u::AbstractMatrix)
     L, N = size(u)
     v = randperm(r, N)
     U = u[:, v]
     bstr_type = L <= 64 ? Int64 : BitBasis.LongLongUInt{L ÷ 64 + 1}
-    sampled = BitStr{L}(0)
-    avail = trues(L)
+    conf = BitStr{L}(0)
     groud_set = collect(1:L)
     # For x1 case, P(x1;m) = |U_{x1, m1}|^2 
     p = abs2.(U[:, 1])
     x_new = sample(r, 1:L, Weights(p))
-    sampled |= indicator(bstr_type, x_new)
-    avail[x_new] = false
+    conf |= indicator(bstr_type, x_new)
     n_vec = normalize([-U[x_new, 2] / U[x_new, 1], 1])
     @inbounds for i in 2:(N - 1)
+        avail = AvailConf(Ref(conf))
         prob = abs2.((view(U, :, 1:i) * n_vec)[avail])
         x_new = sample(r, groud_set[avail], Weights(prob))
-        sampled |= indicator(bstr_type, x_new)
-        avail[x_new] = false
+        conf |= indicator(bstr_type, x_new)
         # now compute next n_vec
         # I suggest not using the gaussian elimination
-        sampled_bool = @. ~ avail
-        U_x = U[sampled_bool, 1:i]
+        U_x = U[SampledConf(Ref(conf)), 1:i]
         B = -U[1:i, i + 1]
         n_vec = normalize([U_x \ B; 1])
     end
+    avail = AvailConf(Ref(conf))
     prob = abs2.((view(U, :, 1:N) * n_vec)[avail])
     x_new = sample(r, groud_set[avail], Weights(prob))
-    sampled |= indicator(bstr_type, x_new)
-    return sampled
+    conf |= indicator(bstr_type, x_new)
+    return conf
 end
 
 newFFS(u::AbstractMatrix) = newFFS(Random.default_rng(), u)
